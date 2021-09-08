af587d8d100c45f78f92e99793565ba9cc0c5da1
= <francois.hamonic@gmail.com>
2021-09-08 14:08:43 +0200

    dynamic prog ok

--- include/knapstack/dynamic_programming.hpp
+++ include/knapstack/dynamic_programming.hpp
@@ -2,50 +2,54 @@
 #define KNAPSTACK_DYNAMIC_PROGRAMMING_HPP
 
 #include <numeric>
-#include <stack>
 #include <type_traits>
-#include <vector>
 
-#include <range/v3/algorithm/remove_if.hpp>
-#include <range/v3/algorithm/sort.hpp>
-#include <range/v3/view/zip.hpp>
+#include <range/v3/view/reverse.hpp>
 
 #include "knapstack/instance.hpp"
 #include "knapstack/solution.hpp"
 
 namespace Knapstack {
-template <typename Value, typename Cost,
-          typename std::enable_if<std::is_integral<Cost>::value>::type>
+template <
+    typename Value, typename Cost,
+    class = typename std::enable_if<std::is_integral<Cost>::value, bool>::type>
 class DynamicProgramming {
 public:
     using TInstance = Instance<Value, Cost>;
     using TItem = typename TInstance::Item;
-    using TSolution = Solution<Inst, Value, Cost>;
+    using TSolution = Solution<Value, Cost>;
 
 public:
     DynamicProgramming() {}
 
     TSolution solve(const TInstance & instance) {
-        TSolution solution(instance);
         const int nb_items = instance.itemCount();
         const Cost budget = instance.getBudget();
 
-        auto tab = std::make_unique<Value[]>((nb_items + 2) * budget);
+        auto tab = std::make_unique<Value[]>((nb_items + 1) * (budget + 1));
 
-        Value * previous_tab = tab.get() + budget;
-        for(int w = 0; w < instance.getBudget(); ++w) {
+        Value * previous_tab = tab.get();
+        for(Cost w = 0; w < budget; ++w) {
             previous_tab[w] = 0;
         }
 
         for(const auto & item : instance.getItems()) {
-            Value * current_tab = previous_tab + budget;
-            for(int w = 0; w < instance.getBudget(); ++w) {
-                current_tab[w] =
-                    std::max(previous_tab[w],
-                             (item.cost > w) *
-                                 (previous_tab[w - item.cost] + item.value));
+            Value * const current_tab = previous_tab + budget + 1;
+            Cost w = 0;
+            for(; w < item.cost; ++w) current_tab[w] = previous_tab[w];
+            for(; w <= budget; ++w) {
+                current_tab[w] = std::max(
+                    previous_tab[w], previous_tab[w - item.cost] + item.value);
             }
             previous_tab = current_tab;
+        }
+
+        TSolution solution(instance);
+        Value * step = previous_tab + budget;
+        for(int i = nb_items - 1; i >= 0; --i) {
+            const bool taken = (*step > *(step - budget - 1));
+            solution.set(i, taken);
+            step -= budget + 1 + taken * instance[i].cost;
         }
 
         return solution;
